206f551057f4a0650aadec136cd8994ed76a2e04
Felipe Bolaños Reyes <83374229+petirgrofin@users.noreply.github.com>
2025-08-12 13:46:51 -0600

    Small fix

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../Components/Navbar";
 
 const allCourses = [
   { id: 1, title: "Introduction to Set Theory" },
